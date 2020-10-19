bdc312cff22a7ddc197174d238745b2588ad64ae
tOlheiser <tolheiser@outlook.com>
2020-10-19 17:58:57 -0600

    Completed.

--- comp255_DiceRollArray/comp255_DiceRollArray/MainWindow.xaml.cs
+++ comp255_DiceRollArray/comp255_DiceRollArray/MainWindow.xaml.cs
@@ -23,59 +23,73 @@ namespace comp255_DiceRollArray
         public MainWindow()
         {
             InitializeComponent();
+            ErrorLabel.Content = ""; // Clear the error label onload
         }
 
         private void RollButton_Click(object sender, RoutedEventArgs e) {
             // initialize variables
             int RollInput;
             int DiceSides;
+            rnd = new Random(); // Create an instance of the random object
 
             // Validate inputs 
             if (!Int32.TryParse(DiceSidesTextbox.Text, out DiceSides)) {
-                // error label
+                ErrorLabel.Content = "Please enter a valid number";
                 return;
             } else if (!Int32.TryParse(DiceRollsTextbox.Text, out RollInput)) {
+                ErrorLabel.Content = "Please enter a valid number";
                 return;
             } else if (DiceSides < 4 || DiceSides > 20) {
+                ErrorLabel.Content = "The sides must fall within 4-20";
                 return;
             } else if (RollInput < 0) {
+                ErrorLabel.Content = "You must enter a positive number";
                 return;
             }
 
+            // Clear the Error Label when all checks have passed
+            ErrorLabel.Content = "";
+
             // initialize arrays
             int[] Side = { DiceSides };
-            int[] RollCount = new int[RollInput]; 
-            
+            int[] RollCount = new int[RollInput];
+
+            // Populate the RollCount array
+            for (int i = 0; i < RollInput; i++) {
+                // Generate a value and store it into the current array index
+                RollCount[i] = rnd.Next(1, DiceSides + 1);
+            }
+
+            // Call the method to show the graph
+            ShowGraph(Side, RollCount);
+        }
+
+        // Create a Random object
+        private static Random rnd;
+
+        // Create a ShowGraph method with two int parameters
+        public void ShowGraph(int[] Labels, int[] Values) {
+            // initialize local variables
+            string GraphOutput;
+            int LabelOutput;
+
+            // Loop to display the labels
+            for (int i = 1; i <= Labels[0]; i++) {
+                LabelOutput = i; // reassign the label on each iteration
+                GraphOutput = ""; // Reset the graph value on each iteration
+
+                // loop over the contents of the Values array
+                for (int j = 0; j < Values.Length; j++) {
+                    // Check that the current label equals the current index of the values array 
+                    if (LabelOutput == Values[j]) {
+                        // increment the label's graph value
+                        GraphOutput += "*";
+                    }
+                }
+
+                // Add a new row to the listbox
+                OutputListbox.Items.Add($"{LabelOutput}\t{GraphOutput}");
+            }
         }
     }
 }
-
-// initialize arrays
-//string[] Months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-//int[] Dataset = { 10, 15, 25, 5, 3, 30, 11, 8, 12, 35, 20, 7 };
-
-// Call Method
-/*
-ShowGraph(Months, Dataset);
-            
-        }
-        public void ShowGraph(string[] Labels, int[] Values)
-{
-    string LabelOutput, GraphOutput; // initialize string variables for the output
-
-    // Looping over the Labels array
-    for (int i = 0; i < Labels.Length; i++)
-    {
-        LabelOutput = Labels[i]; // reassign the label on each iteration
-        GraphOutput = ""; // Reset the graph value on each iteration
-
-        // Add '*' to my graph output so long as the counter is < the current graph value.
-        for (int j = 0; j < Values[i]; j++)
-        {
-            GraphOutput += "*";
-        }
-
-        // Add an item to the listbox
-        OutputListbox.Items.Add($"{LabelOutput}\t{GraphOutput}");
-    } 
-} */
