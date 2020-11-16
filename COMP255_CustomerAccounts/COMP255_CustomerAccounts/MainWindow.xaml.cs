2fcbcb6ecf2aec743d0ebbd887008ffdcce40fab
tOlheiser <tolheiser@outlook.com>
2020-11-15 19:57:42 -0600

    Added comments to click events.

--- COMP255_CustomerAccounts/COMP255_CustomerAccounts/MainWindow.xaml.cs
+++ COMP255_CustomerAccounts/COMP255_CustomerAccounts/MainWindow.xaml.cs
@@ -20,22 +20,68 @@ namespace COMP255_CustomerAccounts {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            // when state is set to "read"
+            // grey out the fields, make them read only
+            // When state is set to "write"
+            // make the textboxes writable & make background white.
+            // Array.Resize(ref myArr, myArr.Length + 1);
         }
 
         private void PreviousButton_Click(object sender, RoutedEventArgs e) {
+            // Get the state, array length, and current index.
 
+            // if state = "read", 
+            if () {
+                // if arraylength = 0 -> return, display message "No accounts. Please submit an account".
+                // if arraylength = 1 -> return, display message "This is the only account"
+                // if arraylength > 1 -> Set the currentIndex to currentIndex - 1.
+                // ---- If currentIndex = 0 -> set the currentIndex to arrayLength
+
+            // state must = "write"
+            } else { 
+                // validate the form inputs
+                // create a new CustomerAccount object & store it in Form's customerAccounts with Array.Resize
+            }
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e) {
+            // Get the state, array length, and current index.
 
+            // if the form state = "read", 
+            if () {
+                // if arraylength = 0 -> return, display message "No accounts. Please submit an account".
+                // if arraylength = 1 -> return, display message "This is the only account"
+                // if arraylength > 1 -> Set the currentIndex to currentIndex - 1.
+                // ---- If currentIndex = arrayLength -> set the currentIndex to 0 
+            }
+            // state must = "write"
+            else
+            {
+                // validate the form inputs
+                // create a new CustomerAccount object & store it in Form's customerAccounts with Array.Resize
+            }
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e) {
-
+            // Clear the inputs
+            // Set form state to "write"
+            // maybe initializer handles if write, do this, if read, do that -> to textboxes
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            // 
 
+            // if form state = read
+            if () {
+                return;
+            // otherwise, validate inputs. 
+            // create a new CustomerAccount object, passing in the inputs.
+            // append the new CustomerAccount object to the form array with Array.Resize
+            // grey out the fields & make textboxes read only
+            } else if () {
+
+            }
         }
     }
     class CustomerAccount {
