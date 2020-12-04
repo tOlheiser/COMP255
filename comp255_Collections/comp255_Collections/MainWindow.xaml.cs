a8808bbe95dc813575e4cf791e3227a6672f2260
tOlheiser <tolheiser@outlook.com>
2020-12-04 17:43:59 -0600

    Everything is working.

--- comp255_Collections/comp255_Collections/MainWindow.xaml.cs
+++ comp255_Collections/comp255_Collections/MainWindow.xaml.cs
@@ -20,21 +20,15 @@ namespace COMP255_CustomerAccounts
     /// </summary>
     public partial class MainWindow : Window {
         // Create a list collection of customer accounts
-        List<CustomerAccount> CustomerAccounts = new List<CustomerAccount>(); //CustomerAccount[] CustomerAccounts;
-
-        // Track the position of the index
-        int CurrentRecordPosition = -1;    //int CurrentRecord;
-
-        // Track the object of the current record
-        CustomerAccount CurrentRecord;
-
-        bool IsNewRecord;
+        List<CustomerAccount> CustomerAccounts = new List<CustomerAccount>(); 
+        int CurrentIndex = -1; // Track the position of the index
+        bool IsNewRecord; 
 
         public MainWindow() {
             InitializeComponent();
 
             // When window is first loaded... 
-            CurrentRecordPosition = 0; // initialize the index
+            CurrentIndex = 0; // initialize the index
 
             // Add to the list collection.
             AddAccount("Jeff", "Spicoli", 1333, 12.75);
@@ -42,7 +36,7 @@ namespace COMP255_CustomerAccounts
             AddAccount("Mike", "Damone", 1650, 200.80);
 
             // Display the first record in the list
-            DisplayRecord(CurrentRecordPosition);
+            DisplayRecords(CurrentIndex);
         } // End MainWindow
 
         private void PreviousButton_Click(object sender, RoutedEventArgs e) {
@@ -50,14 +44,14 @@ namespace COMP255_CustomerAccounts
             if (!SaveRecord()) return;
 
             // Continue to decrement while currentrecord is greater than 0
-            if (CurrentRecordPosition > 0) {
-                CurrentRecordPosition--;
+            if (CurrentIndex > 0) {
+                CurrentIndex--;
             // the index is 0. Set the index to the last element of the list
             } else {
-                CurrentRecordPosition = CustomerAccounts.Count - 1;
+                CurrentIndex = CustomerAccounts.Count - 1;
             }
 
-            DisplayRecord(CurrentRecordPosition);
+            DisplayRecords(CurrentIndex);
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e) {
@@ -65,36 +59,51 @@ namespace COMP255_CustomerAccounts
             if (!SaveRecord()) return;
 
             // continue to increment while CurrentRecord is less than the last index
-            if (CurrentRecordPosition < CustomerAccounts.Count - 1) {
-                CurrentRecordPosition++;
+            if (CurrentIndex < CustomerAccounts.Count - 1) {
+                CurrentIndex++;
             } // otherwise, set index = to the first index.
             else {
-                CurrentRecordPosition = 0;
+                CurrentIndex = 0;
             }
 
-            DisplayRecord(CurrentRecordPosition);
+            DisplayRecords(CurrentIndex);
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e) {
-            // Save record as it is.
-            SaveRecord();
-
-            // Clear the inputs
-            FirstNameTextbox.Clear();
-            LastNameTextbox.Clear();
-            AccountNumberTextbox.Clear();
-            BalanceTextbox.Clear();
-
-            // Update the state of IsNewRecord
-            IsNewRecord = true;
+            SaveRecord(); // Save record as it is.
+            ClearRecords(); // Clear the inputs
+            IsNewRecord = true; // update state
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
             SaveRecord(); // Saves the current record as is.
         }
 
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            // Add a check to make sure there is an item to be deleted.
+            if (CustomerAccounts.Count == 0) {
+                MessageBox.Show($"There are no records to delete.");
+                return;
+            }
+
+            // Remove the record at the current position
+            CustomerAccounts.RemoveAt(CurrentIndex);
+
+            // Make the previous account record the current record
+            CurrentIndex = CurrentIndex - 1 < 0 ? 0 : CurrentIndex - 1;
+
+            // Display the current record
+            if (CustomerAccounts.Count > 0) {
+                DisplayRecords(CurrentIndex);
+            // If there are no records, clear the inputs and update the state of IsNewRecord
+            } else {
+                ClearRecords();
+                IsNewRecord = true;
+            }
+        }
+
         // ===== Methods
-        void DisplayRecord(int RecordNumber) {
+        void DisplayRecords(int RecordNumber) {
             // Displays all the inputs of CustomerAccounts[RecordNumber]
             FirstNameTextbox.Text = CustomerAccounts[RecordNumber].FirstName;
             LastNameTextbox.Text = CustomerAccounts[RecordNumber].LastName;
@@ -102,24 +111,29 @@ namespace COMP255_CustomerAccounts
             BalanceTextbox.Text = CustomerAccounts[RecordNumber].Balance.ToString();
         }
 
+        void ClearRecords() {
+            FirstNameTextbox.Clear();
+            LastNameTextbox.Clear();
+            AccountNumberTextbox.Clear();
+            BalanceTextbox.Clear();
+        }
+
         bool SaveRecord() {
             // Check to see if any fields are blank
-            if (FirstNameTextbox.Text == "" ||
-                LastNameTextbox.Text == "" ||
+            if (FirstNameTextbox.Text == ""     ||
+                LastNameTextbox.Text == ""      ||
                 AccountNumberTextbox.Text == "" ||
                 BalanceTextbox.Text == ""
-               )
-            {
+               ) {
                 MessageBox.Show("All fields must contain a value.");
                 return false;
-
             // if it isn't a new record, update  
             } else if (!IsNewRecord) {
                 // Update the current record of CustomerAccounts
-                CustomerAccounts[CurrentRecordPosition].FirstName = FirstNameTextbox.Text;
-                CustomerAccounts[CurrentRecordPosition].LastName = LastNameTextbox.Text;
-                CustomerAccounts[CurrentRecordPosition].AccountNumber = Convert.ToInt32(AccountNumberTextbox.Text);
-                CustomerAccounts[CurrentRecordPosition].Balance = Convert.ToDouble(BalanceTextbox.Text);
+                CustomerAccounts[CurrentIndex].FirstName = FirstNameTextbox.Text;
+                CustomerAccounts[CurrentIndex].LastName = LastNameTextbox.Text;
+                CustomerAccounts[CurrentIndex].AccountNumber = Convert.ToInt32(AccountNumberTextbox.Text);
+                CustomerAccounts[CurrentIndex].Balance = Convert.ToDouble(BalanceTextbox.Text);
 
                 return true;
             } else {
@@ -128,10 +142,10 @@ namespace COMP255_CustomerAccounts
                     Convert.ToInt32(AccountNumberTextbox.Text), Convert.ToDouble(BalanceTextbox.Text));
 
                 // Set pointer to the new element
-                CurrentRecordPosition = CustomerAccounts.Count - 1;
+                CurrentIndex = CustomerAccounts.Count - 1;
 
                 // Update the values in the window
-                DisplayRecord(CurrentRecordPosition);
+                DisplayRecords(CurrentIndex);
 
                 // Reset the new record flag
                 IsNewRecord = false;
@@ -147,7 +161,7 @@ namespace COMP255_CustomerAccounts
             // Add the temporary placeholder account into the list collection
             CustomerAccounts.Add(TempAccount);
         }
-
+        
     } // End MainWindow Class
 
     class CustomerAccount
