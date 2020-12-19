3438ead886710074b2420b9aab3e84f0d8af1dc7
tOlheiser <tolheiser@outlook.com>
2020-12-19 15:09:32 -0600

    Set up the InvoiceItem class

---
+++ comp255_Final/comp255_Final/InvoiceItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp255_Final {
+    class InvoiceItem {
+        // initialize instance variables
+        private int itemID;
+        private string itemName;
+        private string itemDescription;
+        private int itemQuantity;
+        private double itemPrice;
+
+        // Default Constructor
+        public InvoiceItem() { }
+
+        // Custom Constructor
+        public InvoiceItem(int ItmID, string ItmName, string ItmDescription,
+                           int ItmQuantity, double ItmPrice) {
+            ItemID = ItmID;
+            ItemName = ItmName;
+            ItemDescription = ItmDescription;
+            ItemQuantity = ItmQuantity;
+            ItemPrice = ItmPrice;
+        }
+
+        // Getters and Setters
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public int ItemQuantity { get; set; }
+        public double ItemPrice { get; set; }
+    }
+}
