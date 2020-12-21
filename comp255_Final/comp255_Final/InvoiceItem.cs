71acc5e67b350d2ab74431b8ea3864f6dde094bb
tOlheiser <tolheiser@outlook.com>
2020-12-21 11:54:37 -0600

    Rounded totals to two decimal places

--- comp255_Final/comp255_Final/InvoiceItem.cs
+++ comp255_Final/comp255_Final/InvoiceItem.cs
@@ -8,6 +8,7 @@ namespace comp255_Final {
     class InvoiceItem {
         // initialize instance variables
         private int itemID;
+        private int invoiceID;
         private string itemName;
         private string itemDescription;
         private int itemQuantity;
@@ -17,9 +18,10 @@ namespace comp255_Final {
         public InvoiceItem() { }
 
         // Custom Constructor
-        public InvoiceItem(int ItmID, string ItmName, string ItmDescription,
+        public InvoiceItem(int ItmID, int InvID, string ItmName, string ItmDescription,
                            int ItmQuantity, double ItmPrice) {
             ItemID = ItmID;
+            InvoiceID = InvID;
             ItemName = ItmName;
             ItemDescription = ItmDescription;
             ItemQuantity = ItmQuantity;
@@ -28,9 +30,19 @@ namespace comp255_Final {
 
         // Getters and Setters
         public int ItemID { get; set; }
+        public int InvoiceID { get; set; }
         public string ItemName { get; set; }
         public string ItemDescription { get; set; }
         public int ItemQuantity { get; set; }
         public double ItemPrice { get; set; }
+
+        // Override ToString to display useful info in the listbox
+        // ---> Reference: https://tinyurl.com/lcc96th
+        public override string ToString() {
+            // Test data
+            // Implement formatted string later.
+            return ItemName + " has an ItemID of " + ItemID;
+        }
+
     }
 }
